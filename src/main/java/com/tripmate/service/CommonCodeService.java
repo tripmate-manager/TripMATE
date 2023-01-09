f74420e817b3b492aa3391e53b5f7589b2ce3f07
안알랴줌 <freesens@gmail.com>
2023-01-09 14:06:06 +0900

    logback 적용 및 api url 변경

--- src/main/java/com/tripmate/service/CommonCodeService.java
+++ src/main/java/com/tripmate/service/CommonCodeService.java
@@ -3,15 +3,15 @@ package com.tripmate.service;
 import com.tripmate.domain.CommonDetailCodeVO;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 import java.util.List;
 
 public interface CommonCodeService {
-    @GET("v1/common/codeList")
-    Call<List<CommonDetailCodeVO>> searchCommonDetailCodeList(@Query("commonCode") String commCd);
+    @GET("v1/common/codes/{commonCode}")
+    Call<List<CommonDetailCodeVO>> searchCommonDetailCodeList(@Path("commonCode") String commCd);
 
-    @GET("v1/common/code")
-    Call<CommonDetailCodeVO> getCommonDetailCode(@Query("commonCode") String commCd,
-                                                 @Query("commonDetailCode") String commDtlCd);
+    @GET("v1/common/codes/{commonCode}/{commonDetailCode}")
+    Call<CommonDetailCodeVO> getCommonDetailCode(@Path("commonCode") String commCd,
+                                                 @Path("commonDetailCode") String commDtlCd);
 }
