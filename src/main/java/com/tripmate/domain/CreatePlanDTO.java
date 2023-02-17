94c149b811763984fd200f8e60a570576bb682f9
최지연 <cjyeon@pionnet.co.kr>
2023-02-17 14:30:30 +0900

    - CreatePlanDTO > validation Annotation 수정

--- src/main/java/com/tripmate/domain/CreatePlanDTO.java
+++ src/main/java/com/tripmate/domain/CreatePlanDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.List;
@@ -42,7 +42,7 @@ public class CreatePlanDTO {
     @Pattern(regexp = "(19|20)\\d{2}.(0[1-9]|1[012]).(0[1-9]|[12][0-9]|3[01])", message = "여행 종료 일자는 'yyyy.MM.dd' 형태로 입력가능합니다.")
     private String tripEndDate;
 
-    @NotNull(message = "여행지를 입력해주세요.")
+    @NotEmpty(message = "여행지를 입력해주세요.")
     private List<Integer> planAddressList;
 
     private List<Integer> planThemeList;
