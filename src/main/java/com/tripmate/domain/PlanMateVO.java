d51f55451665632df55903dd8f1eaa9381f259e2
최지연 <cjyeon@pionnet.co.kr>
2023-02-28 09:25:32 +0900

    - 플랜조회 기능 추가 - 플랜 메이트 조회 기능 추가 - 플랜 수정 기능 추가

---
+++ src/main/java/com/tripmate/domain/PlanMateVO.java
@@ -0,0 +1,29 @@
+package com.tripmate.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class PlanMateVO {
+    @NotBlank(message = "플랜번호를 입력해주세요.")
+    private int planNo;
+
+    @NotBlank(message = "회원번호를 입력해주세요.")
+    private int memberNo;
+
+    @NotBlank(message = "플랜리더여부를 입력해주세요.")
+    @Pattern(regexp = "^[YN]$", message = "플랜 리더(생성자) 여부는 Y, N만 입력 가능합니다.")
+    private String leadYn;
+
+    private String memberName;
+
+    private String nickName;
+}
--- src/main/java/com/tripmate/service/apiservice/PlanApiService.java
+++ src/main/java/com/tripmate/service/apiservice/PlanApiService.java
@@ -1,8 +1,9 @@
 package com.tripmate.service.apiservice;
 
-import com.tripmate.domain.CreatePlanDTO;
 import com.tripmate.domain.PlanAddressVO;
 import com.tripmate.domain.PlanAttributeVO;
+import com.tripmate.domain.PlanDTO;
+import com.tripmate.domain.PlanMateVO;
 import com.tripmate.domain.PlanVO;
 
 import java.util.List;
@@ -11,6 +12,9 @@ public interface PlanApiService {
     List<PlanAttributeVO> searchPlanAttributeList(String attributeTypeCode) throws Exception;
     List<PlanAddressVO> searchAddressList() throws Exception;
     List<PlanAddressVO> searchAddressList(String sidoName) throws Exception;
-    boolean createPlan(CreatePlanDTO createPlanDTO) throws Exception;
+    boolean createPlan(PlanDTO planDTO) throws Exception;
     List<PlanVO> searchMemberPlanList(String memberNo) throws Exception;
+    PlanVO getPlanInfo(String memberNo) throws Exception;
+    List<PlanMateVO> searchPlanMateList(String planNo) throws Exception;
+    boolean updatePlan(String planNo, PlanDTO planDTO) throws Exception;
 }
