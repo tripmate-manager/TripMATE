f094328712118da85e0ad52fe2ef93871d048543
최지연 <cjyeon@pionnet.co.kr>
2023-04-04 11:25:24 +0900

    - DailyPlanCntVO/DailyPlanVO > 불필요 Valid annotation 제거

--- src/main/java/com/tripmate/domain/DailyPlanCntVO.java
+++ src/main/java/com/tripmate/domain/DailyPlanCntVO.java
@@ -5,16 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class DailyPlanCntVO {
-    @NotBlank
     private String dayGroup;
 
-    @NotBlank
     private String dailyPlanCnt;
 }
--- src/main/java/com/tripmate/domain/DailyPlanVO.java
+++ src/main/java/com/tripmate/domain/DailyPlanVO.java
@@ -5,44 +5,32 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 public class DailyPlanVO {
-
-    @NotBlank
     private String dailyPlanNo;
 
-    @NotBlank
     private String planNo;
 
-    @NotBlank
     private String postNo;
 
-    @NotBlank
     private String dayGroupNo;
 
-    @NotBlank
     private String dailyPlanDateTime;
 
-    @NotBlank
     private String notificationYn;
 
     private String reviewAverageScore;
 
     private String postTypeCode;
 
-    @NotBlank
     private String postTitle;
 
-    @NotBlank
     private String postContents;
 
     private String spotAddress;
 
-    @NotBlank
     private String postRegistrationNo;
 }
